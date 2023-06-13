447f536c8e8776487123d9b9a840166e051192a8
Jmorenos2022 <129952721+Jmorenos2022@users.noreply.github.com>
2023-06-13 16:23:52 +0200

    Delete ProyectoBD directory

--- ProyectoBD/Datos/Carreras_202306101630.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO ClubHipica.Carreras (orden,premio,categoria,hora,fecha,nombre_comp) VALUES
-	 (2,1904,'B','09:44:17','2022-12-24','Portugal'),
-	 (3,1557,'A','04:57:25','2022-08-25','Portugal'),
-	 (3,1775,'B','04:45:20','2023-02-24','Madrid');
--- ProyectoBD/Datos/Ediciones_202306101630.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO ClubHipica.Ediciones (fecha,nombre_comp) VALUES
-	 ('2023-02-24','Madrid'),
-	 ('2022-08-25','Portugal'),
-	 ('2022-12-24','Portugal');

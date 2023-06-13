dbff37caacb20572d86de650c96459873e016800
Jmorenos2022 <129952721+Jmorenos2022@users.noreply.github.com>
2023-06-13 16:24:15 +0200

    Add BD

---
+++ ProyectoBD/Datos/Competiciones_202306101630.sql
@@ -0,0 +1,8 @@
+INSERT INTO ClubHipica.Competiciones (nombre,lugar,pais,tipo) VALUES
+	 ('Badajoz','Zoetermeer','France','penatibus et'),
+	 ('Barcelona','Bạc Liêu','South Africa','quam'),
+	 ('Caceres','Tourcoing','Italy','pede sagittis augue,'),
+	 ('Cadiz','Pamplona','Austria','arcu vel quam'),
+	 ('Madrid','Shillong','Peru','enim.'),
+	 ('Portugal','Uikhoven','Germany','a'),
+	 ('Sevilla','Piotrków Trybunalski','Nigeria','euismod in,');
---
+++ ProyectoBD/Datos/Resultados_202306101630.sql
@@ -0,0 +1,4 @@
+INSERT INTO ClubHipica.Resultados (id_jinete,id_caballo,cajon,tiempo,orden,fecha,nombre_comp) VALUES
+	 (1,1,1,'00:05:30',3,'2023-02-24','Madrid'),
+	 (2,2,3,'00:46:32',3,'2022-08-25','Portugal'),
+	 (3,3,2,'00:47:40',2,'2022-12-24','Portugal');
